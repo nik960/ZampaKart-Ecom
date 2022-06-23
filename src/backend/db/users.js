938cf175f0365ed85021546b5d0b5353facd1421
Nikhil Kumar <nikhil957022@gmail.com>
2022-06-23 16:09:41 +0530

    Added Successfully

---
+++ src/backend/db/users.js
@@ -0,0 +1,21 @@
+import { v4 as uuid } from "uuid";
+// import bcyrpt from "bcryptjs";
+import { formatDate } from "../utils/authUtils";
+/**
+ * User Database can be added here.
+ * You can add default users of your wish with different attributes
+ * Every user will have cart (Quantity of all Products in Cart is set to 1 by default), wishList by default
+ * */
+
+export const users = [
+  {
+    _id: uuid(),
+    firstName: "Nikhil",
+    lastName: "Kumar",
+    email: "NIKHIL@123",
+    password:123,
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  
+];
